e9688a882e953e0f2b5addc16e45e223a1cb6f5b
arthurleoheteck <s10240060@connect.np.edu.sg>
2023-01-25 21:28:01 +0800

    added in doc string

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import coh, overheads, profit_loss
 
 #import the collaborate code and modulizrized python files
 # each api.py, coh.py, overheads.py, profit_loss.py can be run separately for each member to contribute to the project
-
+"""
+This code imports the modules coh, overheads, and profit_loss and runs their respective functions, coh_function, overhead_function,
+ and profitloss_function, with the variable forex passed as an argument. The variable forex is set to 1 and represents the exchange 
+ rate for USD. Each module, coh.py, overheads.py, and profit_loss.py can be run separately by members contributing to the project. 
+ The main() function is executed to run the overall program.
+"""
 def main():
 
     # updated exercise set forex  = 1, USD 

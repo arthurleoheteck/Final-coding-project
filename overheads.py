253050d889cc9733244f888fefe314508a78afed
arthurleoheteck <s10240060@connect.np.edu.sg>
2023-01-31 20:39:42 +0800

    code and comment update

--- overheads.py
+++ overheads.py
@@ -16,16 +16,27 @@ def overhead_function(forex):
         max_overheads = 0.00
 
         # read each record and store the only highest overheads and category
-        for row in reader:            
-        #compare and keep the maximum category and overheads
-        #row[0] for column 0 category, row[1] for column 1 overheads
+        count = 0
+        for row in reader: 
+        
+            #check if row is empty. If can be a return or new line. 
+            if not row:
+                continue
+
+            #Store the first record as max category and max overheads
+            #row[0] for column 0 category, row[1] for column 1 overheads
+            if count == 0:
+                max_category    = row[0]
+                max_overheads   = float(row[1])
+                count = count + 1
+            
+            #compare and keep the maximum category and overheads
             if  float(row[1]) > max_overheads:
                 max_category    = row[0]
                 max_overheads   = float(row[1])
-        
+
         file.close()
         
-            
     txt_return = "[HIGHEST OVERHEADS] {}: {:.2f}%\n".format(max_category.upper(), max_overheads)
     print(txt_return)
     
